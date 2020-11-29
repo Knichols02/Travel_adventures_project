fd2510e5439a3bdc9dcbcd96f523a46187896e7a
Kirsten Nicholson <kirstennic6@gmail.com>
2020-11-29 18:54:25 +0000

    added create route in cities

--- controllers/city_controller.py
+++ controllers/city_controller.py
@@ -2,6 +2,7 @@ from flask import Flask, Blueprint, render_template, redirect, request
 from models.city import City 
 from repositories import city_repository
 import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
 
 city_blueprint = Blueprint("city", __name__)
 
@@ -13,15 +14,30 @@ def cities():
     return render_template("cities/index.html", all_cities = cities)
 
 #NEW 
-# GET '/cities/new'
+# GET '/cities/new' --> to create a new city 
+@city_blueprint.route("/cities/new", methods= ["GET"])
+def new_city():
+    countries = country_repository.select_all()
+    return render_template ("cities/new.html", all_countries=countries)
 
 #CREATE
-#POST - '/cities'
+#POST - '/cities' --> to handle to post from the new 
+@city_blueprint.route("/cities", methods= ["POST"])
+def create_city():
+    #data from form 
+    city_name = request.form['city_name']
+    date_of_travel = request.form['date_of_travel']
+    visited = request.form['visited']
 
-#form data 
-#select country 
-#create a new city
-#save the city to the database(with save method)
+    #select the country using the repository 
+    country = country_repository.select(country_id)
+    
+    #create a new city
+    city = City(city_name, date_of_travel, visited)
+
+    #save the city back to the database with the save method
+    city_repository.save(city)
+    return redirect("/cities")
 
 
 #SHOW
