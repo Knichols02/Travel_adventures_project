d09d17c35b5445612d12fc412b979a8d8a2a0a36
Kirsten Nicholson <kirstennic6@gmail.com>
2020-11-29 13:03:04 +0000

    created save, delete and update Crud actions

--- repositories/city_repository.py
+++ repositories/city_repository.py
@@ -4,7 +4,7 @@ from db.run_sql import run_sql
 from models.city import City
 from models.country import Country
 import repositories.country_repository as country_repository
-
+import repositories.city_repository as city_repository
 
 #CREATE/SAVE  
 def save(city):
@@ -20,13 +20,38 @@ def select_all():
 
     sql = "SELECT * FROM cities"
     results = run_sql(sql)
+
     for row in results:
-        city = City
+        country = country_repository.select(row['country_id'])
+        city = City(row['city_name'], row['date_of_travel'], row['visited'], row['id'])
+        cities.append(city)
+    return cities
 
 #READ - SELECT ONE 
+def select(id):
+    city = None 
+    sql = "SELECT * FROM cities WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values) [0]
+
+    if result is not None:
+        country = country_repository.select(result['country_id]'])
+        city = City(result['city_name'], result['date_of_travel'], result['visited'], result['id'])
+    return city 
 
 #DELETE - DELETE ALL 
+def delete_all():
+    sql = "DELETE FROM cities"
+    run_sql(sql)
 
 #DELETE - DELETE ONE 
+def delete(id):
+    sql = "DELETE FROM cities WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
 
 #UPDATE (ONE)
+def update(city):
+    sql = "UPDATE cities SET (city_name, date_of_travel, visited) = (%s, %s, %s) WHERE id = %s"
+    values = [city.city_name, city.date_of_travel, city.visited]
+    run_sql(sql, values)
--- repositories/country_repository.py
+++ repositories/country_repository.py
@@ -4,17 +4,34 @@ from db.run_sql import run_sql
 from models.country import Country 
 from models.city import City
 import repositories.country_repository as country_repository
+import repositories.city_repository as city_repository
 
 #CREATE/SAVE  
-def save(country_to_save):
+def save(country):
     sql = "INSERT INTO countries(name) VALUES (%s) RETURNING id"
-    values = []
+    values = [country.name]
+    results = run_sql(sql, values)
+    id = results[0] ['id']
+    country.id = id
+    return country 
+
 #READ - SELECT ALL 
 
 #READ - SELECT ONE 
 
 #DELETE - DELETE ALL 
+def delete_all():
+    sql = "DELETE FROM countries"
+    run_sql(sql)
 
 #DELETE - DELETE ONE 
+def delete(id):
+    sql = "DELETE FROM countries WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
 
 #UPDATE (ONE)
+def update(country):
+    sql = "UPDATE countries SET (name) = (%s) WHERE id = %s"
+    values = [country.name]
+    run_sql(sql, values)
